564401c2c1bb9a6ffd798c8397ad73603deef65f
deep <deepjadav1543@gmail.com>
2025-05-27 14:56:30 +0530

    Prepare for deployment to Render

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 // MySQL Connection Pool
 dotenv.config();
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
+    host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
