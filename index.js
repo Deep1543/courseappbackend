38b9fff3c3263743e09e596fbfbbeb11d2dd920e
deep <deepjadav1543@gmail.com>
2025-05-27 14:35:32 +0530

    Prepare for deployment to Render

--- index.js
+++ index.js
@@ -26,15 +26,16 @@ app.use(cors({ origin: '*', credentials: true }));
 app.use(bodyParser.json());
 
 // MySQL Connection Pool
+dotenv.config();
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'deep1543',
-    database: 'courseapp',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-});
+  });
 
 const handleDbError = (error, res) => {
     console.error('Database error:', error);
